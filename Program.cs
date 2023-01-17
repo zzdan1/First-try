00f80bf1dfa38645b9c0b88ceb3aff646067114a
Данил Анциферов <zz-dan1@yandex.ru>
2023-01-17 17:34:58 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        begin:;
+            Console.WriteLine("Введите два числа, которые хотите перемножить");
+            int num1 = Convert.ToInt32 (Console.ReadLine());
+            int num2 = Convert.ToInt32 (Console.ReadLine());
+            num1 = num1 * num2;
+            Console.WriteLine(num1);
+            Console.WriteLine("Выйти? Да-yes");
+            string c = Console.ReadLine();
+            if(c!="yes")
+            {
+                goto begin;
+            }
+        }
+
+    }
+}
